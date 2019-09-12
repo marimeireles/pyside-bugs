af1f0f6f84c73dc3593b962e2d1b81d6b40ca30d
Mariana Meireles <mariana@psychonautgirl.space>
2019-09-12 12:43:37 +0200

    adds bug 1060

--- 1060/example.py
+++ 1060/example.py
@@ -4,8 +4,8 @@ from PySide2 import QtCore, QtWidgets, QtGui
 os.environ["QT_SCREEN_SCALE_FACTORS"] = "2"
 
 app = QtWidgets.QApplication(["tray_icon_test"])
-app.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps)
-app.setQuitOnLastWindowClosed(False)
+# app.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps)
+# app.setQuitOnLastWindowClosed(False)
 
 icon_file = os.path.join(os.path.dirname(__file__), "example.svg")
 print(icon_file)
